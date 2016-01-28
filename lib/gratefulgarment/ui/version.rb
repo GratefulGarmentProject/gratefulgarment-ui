027cef970864121f49794262e013327d8650f017
Mike Virata-Stone <mjstone@on-site.com>
2016-01-28 12:03:14 -0800

    Fix rubocop

--- lib/gratefulgarment/ui/version.rb
+++ lib/gratefulgarment/ui/version.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 module Gratefulgarment
   module Ui
-    VERSION = "0.1.0"
+    VERSION = "0.1.0".freeze
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'gratefulgarment/ui'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "gratefulgarment/ui"
